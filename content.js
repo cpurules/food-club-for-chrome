31eb4e46c1ac264d58ba41bc2488cf073b1c25b5
Charlie Unfricht <dzcharlie@gmail.com>
2019-12-14 12:18:31 -0500

    Add logic to pull bet info from ~boochi_target

--- content.js
+++ content.js
@@ -1,3 +1,12 @@
-chrome.webNavigation.onCompleted.addListener(function() {
-    alert("You're on a matching pet page!");
-}, {url: [{urlMatches : 'http://www.neopets.com/~boochi_target'}]});
+var betTable = document.getElementsByTagName("table")[2];
+var tableRows = betTable.querySelectorAll("tr");
+
+// The first two table rows are the headers; the last table row is possible winnings.
+// We will want to skip all three of these since they don't contain any bets.
+for(var i = 2; i < tableRows.length - 1; i++) {
+    betRow = tableRows[i];
+    var betData = betRow.querySelectorAll("td");
+
+    var betInfoCell = betData[1];
+    var betInfo = betInfoCell.innerHTML;
+}
