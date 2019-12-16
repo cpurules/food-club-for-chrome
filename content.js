bf579760d55af471ca462a72a07b4b7c238a786d
Charlie Unfricht <dzcharlie@gmail.com>
2019-12-15 19:10:41 -0500

    Add handling for no bets placed

--- content.js
+++ content.js
@@ -1,12 +1,27 @@
-var betTable = document.getElementsByTagName("table")[2];
-var tableRows = betTable.querySelectorAll("tr");
+if(window.location.href.endsWith("boochi_target")) {
+    var betTable = document.getElementsByTagName("table")[2];
+    var tableRows = betTable.querySelectorAll("tr");
 
-// The first two table rows are the headers; the last table row is possible winnings.
-// We will want to skip all three of these since they don't contain any bets.
-for(var i = 2; i < tableRows.length - 1; i++) {
-    betRow = tableRows[i];
-    var betData = betRow.querySelectorAll("td");
+    // The first two table rows are the headers.  We will skip these always.
+    // If there are bets, the last table row is possible winnings, which we don't need
+    // We will skip these
+    for(var i = 2; i < tableRows.length; i++) {
+        // If there are bets, tableRows.length > 3
+        // So if we're at the end of the table, and tableRows.length > 3, it is the
+        // possible winnings row that we can discard
+        if(i > 2 && i == (tableRows.length - 1)) {
+            break;
+        }
 
-    var betInfoCell = betData[1];
-    var betInfo = betInfoCell.innerHTML;
+        betRow = tableRows[i];
+        var betData = betRow.querySelectorAll("td");
+
+        // check for no bets
+        if(i == 2 && betData[0].innerHTML == "You do not have any bets placed for this round!") {
+            return;
+        }
+    
+        var betInfoCell = betData[1];
+        var betInfo = betInfoCell.innerHTML;
+    }
 }
