fc3f511b5aee730199383d54b99a65a71d395bc5
Charlie Unfricht <dzcharlie@gmail.com>
2019-12-15 20:38:01 -0500

    Re-add boochi_target logic (work-in-progress)

--- content.js
+++ content.js
@@ -64,7 +64,33 @@ function setArenaBet(betObject, arena) {
     }
 }
 
-if(window.location.href.endsWith("~HGB")) {
+if(window.location.href.endsWith("boochi_target")) {
+    var betTable = document.getElementsByTagName("table")[2];
+    var tableRows = betTable.querySelectorAll("tr");
+
+    var betButton = document.createElement("button");
+    var betText = document.createTextNode("Place bet");
+    betButton.appendChild(betText);
+    betTable.insertBefore(betButton, betTable.childNodes[0]);
+
+    var placedBets = false;
+
+    // If the 3rd row in the table is a single cell containing the text:
+    //         You do not have any bets placed for this round!
+    // ...then no bets were placed.
+    placedBets = (tableRows[2].querySelector("td").innerHTML != "You do not have any bets placed for this round!");
+    
+    if(true) { // CHANGE TO IF(placedBets) LATER!!!!
+        for(var i = 2; i < tableRows.length; i++) {   
+            var betRow = tableRows[i];
+            var betData = betRow.querySelectorAll("td");
+        
+            var betInfoCell = betData[1];
+            var betInfo = betInfoCell.innerHTML;
+        }
+    }
+}
+else if(window.location.href.endsWith("~HGB")) {
     // HGB's HTML doesn't lay this out the same way the DOM interprets it
     // Use "Inspect Element" functionality to determine the table data
     var betTable = document.getElementsByTagName("table")[1];
