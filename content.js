8cf68759ebc1c5e06540743e0d4d15b7293c19ab
Charlie Unfricht <dzcharlie@gmail.com>
2019-12-15 19:17:24 -0500

    Refactor loop for bet pickup

--- content.js
+++ content.js
@@ -14,26 +14,13 @@ if(window.location.href.endsWith("boochi_target")) {
     // ...then no bets were placed.
     placedBets = (tableRows[2].querySelector("td").innerHTML != "You do not have any bets placed for this round!");
     
-    // The first two table rows are the headers.  We will skip these always.
-    // If there are bets, the last table row is possible winnings, which we don't need
-    // We will skip these
-    for(var i = 2; i < tableRows.length; i++) {
-        // If there are bets, tableRows.length > 3
-        // So if we're at the end of the table, and tableRows.length > 3, it is the
-        // possible winnings row that we can discard
-        if(i > 2 && i == (tableRows.length - 1)) {
-            break;
+    if(true) { // CHANGE TO IF(placedBets) LATER!!!!
+        for(var i = 2; i < tableRows.length; i++) {   
+            betRow = tableRows[i];
+            var betData = betRow.querySelectorAll("td");
+        
+            var betInfoCell = betData[1];
+            var betInfo = betInfoCell.innerHTML;
         }
-
-        betRow = tableRows[i];
-        var betData = betRow.querySelectorAll("td");
-
-        // check for no bets
-        if(i == 2 && betData[0].innerHTML == "You do not have any bets placed for this round!") {
-            break;
-        }
-    
-        var betInfoCell = betData[1];
-        var betInfo = betInfoCell.innerHTML;
     }
 }
